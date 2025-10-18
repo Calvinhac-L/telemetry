3688e14ef12c378ea9558cba62c47e4143b49884
LouisC <74770627+Calvinhac-L@users.noreply.github.com>
2025-10-18 17:48:46 +0200

    SPRINT #1 - Jour 1 (#1)
    * chore(*): rm app folder, refined pydantic Settings + .env

--- backend/api/telemetry.py
+++ backend/api/telemetry.py
@@ -12,12 +12,12 @@ from db.models import Telemetry
 
 telemetry_router = APIRouter()
 
-@telemetry_router.get("/telemetry")
+@telemetry_router.get("/telemetry", tags=["telemetry"])
 def get_telemetry():
     today = datetime.now().date()
     return {f"telemetry_{today}" : "TEST"}
 
-@telemetry_router.post("/telemetry")
+@telemetry_router.post("/telemetry", tags=["telemetry"])
 def add_telemetry(telemetry: Telemetry):
     telemetry.id = uuid.uuid4()
     telemetry.timestamp = datetime.now().timestamp()
---
+++ backend/core/config.py
@@ -0,0 +1,19 @@
+﻿"""
+Fichier de configuration de l'environnement de l'application
+"""
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    app_name: str = ""
+    database_url: str = ""
+    debug: bool = False
+
+    model_config = SettingsConfigDict(
+        env_file='.env.example',
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
+
+settings = Settings()
