4ff1e9310a87b8a592c1eb2e8fd0f077e409f4a2
Calvinhac-L <louis.calvinhac@gmail.com>
2025-11-04 11:26:40 +0100

    feat(ws): ajout websocket

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from fastapi import FastAPI
 
 from api.telemetry import telemetry_router
+from api.ws import ws_router
 from api.root import root_router
 from db.database import Base, engine
 
@@ -15,4 +16,5 @@ if not Path("telemetry.db").exists():
     Base.metadata.create_all(bind=engine)
 
 app.include_router(telemetry_router, tags=["Telemetry"])
+app.include_router(ws_router, tags=["WebSocket"])
 app.include_router(root_router)
